380f2d6f3b131cb153d8577df9da125f0f8ff026
linta <lintashijo@gmail.com>
2020-06-06 00:49:04 -0400

    hfkyhj

---
+++ src/test/java/MyMavenProjects/AppiumTest.java
@@ -0,0 +1,12 @@
+package MyMavenProjects;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeApp() {
+		System.out.println("NativeApp");
+	}
+
+}
---
+++ src/test/java/MyMavenProjects/RESTAPITest.java
@@ -0,0 +1,19 @@
+package MyMavenProjects;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("Postjira");
+	}
+	@Test
+	public void deleteTwitter() {
+		
+		System.out.println("iosapp");
+	}
+	
+
+}
